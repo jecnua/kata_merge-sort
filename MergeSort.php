e108a208e305c59334660e3d3763f4e40ee05cab
jecnua <jecnua@gmail.com>
2014-07-01 19:29:01 +0100

    Started the merging of two array while sorting

--- MergeSort.php
+++ MergeSort.php
@@ -11,14 +11,29 @@ namespace algorithm;
 
 class MergeSort {
 
-    public function merge_sort(array $mergeMe){
-
-        if (count($mergeMe) <= 1) {
-            return $mergeMe;
+    public function merge_sort(array $arrayToSort){
+        if (count($arrayToSort) <= 1) {
+            return $arrayToSort;
         }
+        //Split
+        //Merge
+        $sorted = $this->merge_two_arrays(array(3,9),array(6,1));
+        return $sorted;
+    }
 
-        $merged = array();
-        return $merged;
+    public function merge_two_arrays($first, $second){
+        $result = array();
+        while (count ($first) != 0 && count ($second) != 0){
+            if ($first[0] <= $second[0]){
+                $result[] = $first[0];
+                array_shift($first);
+            }
+            else {
+                $result[] = $second[0];
+                array_shift($second);
+            }
+        }
+        return $result;
     }
 
 } 
