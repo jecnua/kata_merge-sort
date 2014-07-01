7826fe25bee07c921b0f9d94081444fea59cb3af
jecnua <jecnua@gmail.com>
2014-07-01 19:08:48 +0100

    Trying PhpStorm

---
+++ MergeSort.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jecnua
+ * Date: 01/07/14
+ * Time: 18:58
+ */
+
+namespace algorithm;
+
+
+class MergeSort {
+
+    public function foo(){
+        echo 'test';
+    }
+
+} 
---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jecnua
+ * Date: 01/07/14
+ * Time: 19:04
+ */
+
+include_once 'MergeSort.php';
+
+$alg = new \algorithm\MergeSort();
+$alg->foo();
